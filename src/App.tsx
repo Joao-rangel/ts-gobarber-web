f6daa43f6b44ed59e966d7973b73681cf0bb7b67
Joao Rangel <joao.vran@gmail.com>
2020-11-20 17:09:56 -0300

    add error validation on sign-in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SignUp from './pages/SignUp/index';
+import SignIn from './pages/SignIn/index';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <SignIn />
     <GlobalStyle />
   </>
 );
