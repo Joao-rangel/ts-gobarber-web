62ee073452d558f6cf6b50b6011df9b6433ff9e8
Joao Rangel <joao.vran@gmail.com>
2020-11-20 18:34:38 -0300

    initiated useContext for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn/index';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'João' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
