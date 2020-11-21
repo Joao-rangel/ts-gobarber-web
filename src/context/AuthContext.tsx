66d4fd256179759f92d04306657c2b7f0567f895
Joao Rangel <joao.vran@gmail.com>
2020-11-21 11:52:48 -0300

    signOut() created in AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -14,6 +14,7 @@ interface SignInCredentials {
 interface AuthContextData {
   user: Record<string, unknown>;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -47,8 +48,15 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({ token, user });
   }, []);
 
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@Gobarber:token');
+    localStorage.removeItem('@Gobarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
