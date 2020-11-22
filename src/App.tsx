475e6a64504eb5930c2182bac6021f1e82dd0b0b
Joao Rangel <joao.vran@gmail.com>
2020-11-22 13:48:40 -0300

    add router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
-import SignIn from './pages/SignIn/index';
 
 import AppProvider from './Hooks/index';
 
+import Routes from './routes';
+
 const App: React.FC = () => (
-  <>
+  <Router>
     <AppProvider>
-      <SignIn />
+      <Routes />
     </AppProvider>
 
     <GlobalStyle />
-  </>
+  </Router>
 );
 
 export default App;
