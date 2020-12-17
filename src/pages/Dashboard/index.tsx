3d86281bf55ac2bef25d3faab0298f60c1fada81
Joao Rangel <joao.vran@gmail.com>
2020-12-16 23:41:00 -0300

    add appointments cards in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import React, { useState } from 'react';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 
 import LogoImg from '../../assets/logo.svg';
 import { useAuth } from '../../Hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -27,6 +40,80 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Próximo atendimento</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/59208261?s=460&u=ec733620de214fef7f2ce938d6bc22a80d145b02&v=4"
+                alt="João Rangel"
+              />
+              <strong>João Rangel</strong>
+              <span>
+                <FiClock />
+                8:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                8:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/59208261?s=460&u=ec733620de214fef7f2ce938d6bc22a80d145b02&v=4"
+                  alt="João Rangel"
+                />
+                <strong>João Rangel</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                8:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/59208261?s=460&u=ec733620de214fef7f2ce938d6bc22a80d145b02&v=4"
+                  alt="João Rangel"
+                />
+                <strong>João Rangel</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                8:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/59208261?s=460&u=ec733620de214fef7f2ce938d6bc22a80d145b02&v=4"
+                  alt="João Rangel"
+                />
+                <strong>João Rangel</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
